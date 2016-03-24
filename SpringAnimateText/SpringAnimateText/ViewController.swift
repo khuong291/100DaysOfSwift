5b8ddfea200231585fb43a4bc8a2247d8c12dc15
Khuong Pham <dkhuong291@gmail.com>
2016-03-24 11:30:12 +0700

    Finish Project SpringAnimateText

--- SpringAnimateText/SpringAnimateText/ViewController.swift
+++ SpringAnimateText/SpringAnimateText/ViewController.swift
@@ -10,25 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var myFirstLabel: UILabel!
-    var mySecondLabel: UILabel!
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        myFirstLabel = UILabel()
+        let button = UIButton(type: UIButtonType.System) as UIButton
+        button.bounds = CGRect(x: 0, y: 0, width: 250, height: 30)
+        button.center = view.center
+        button.backgroundColor = UIColor.greenColor()
+        button.setTitle("Please, DONT CLICK HERE!!", forState: UIControlState.Normal)
+        button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+
+        self.view.addSubview(button)
+    }
+
+    func buttonAction(button: UIButton) {
+        let myFirstLabel = UILabel()
         myFirstLabel.text = "Hello"
         myFirstLabel.font = UIFont.systemFontOfSize(36)
         myFirstLabel.sizeToFit()
         myFirstLabel.center = CGPoint(x: 100, y: 40)
         view.addSubview(myFirstLabel)
 
-        mySecondLabel = UILabel()
+        UIView.animateWithDuration(1.0, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.0, options: [], animations: {
+            myFirstLabel.center = CGPoint(x: 100, y: 40 + 200)
+            }, completion: nil)
+
+        let mySecondLabel = UILabel()
         mySecondLabel.text = "I'm Khuong"
         mySecondLabel.font = UIFont.boldSystemFontOfSize(48)
         mySecondLabel.sizeToFit()
         mySecondLabel.center = CGPoint(x: 200, y: 90)
+        mySecondLabel.alpha = 0.0
         view.addSubview(mySecondLabel)
+
+        UIView.animateWithDuration(3.0, delay: 1.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+            mySecondLabel.center = CGPoint(x: 200, y: 90 + 200)
+            mySecondLabel.alpha = 1
+            }) {(_) -> Void in
+                button.alpha = 0.0
+        }
     }
 
     override func didReceiveMemoryWarning() {
