581a2a0670240768ca9145f5e0e1913b962f5e6d
Khuong Pham <dkhuong291@gmail.com>
2016-03-25 23:40:58 +0700

    Create Project ShowPopverController

---
+++ ShowPopoverController/ShowPopoverController/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ShowPopoverController
+//
+//  Created by Khuong Pham on 3/25/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+    }
+
+}
+
