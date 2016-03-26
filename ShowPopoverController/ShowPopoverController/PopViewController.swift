42fa86517943cfadf78364a99db04edac42d8570
Khuong Pham <dkhuong291@gmail.com>
2016-03-26 12:16:02 +0700

    Finish Project ShowPopoverController

---
+++ ShowPopoverController/ShowPopoverController/PopViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PopViewController.swift
+//  ShowPopoverController
+//
+//  Created by Khuong Pham on 3/26/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class PopViewController: UIViewController {
+
+    
+    
+}
--- ShowPopoverController/ShowPopoverController/ViewController.swift
+++ ShowPopoverController/ShowPopoverController/ViewController.swift
@@ -8,15 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func buttonTapped(sender: AnyObject) {
-        
+    
+    @IBAction func barButtonTapped(sender: UIBarButtonItem) {
+        let popVC = storyboard?.instantiateViewControllerWithIdentifier("PopViewController") as! PopViewController
+        popVC.preferredContentSize = CGSize(width: UIScreen.mainScreen().bounds.width, height: 300)
+        let navController = UINavigationController(rootViewController: popVC)
+        navController.modalPresentationStyle = .Popover
+
+        let popOver = navController.popoverPresentationController
+        popOver?.delegate = self
+        popOver?.barButtonItem = sender
+        presentViewController(navController, animated: true, completion: nil)
+
     }
 
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .None
+    }
 }
 
