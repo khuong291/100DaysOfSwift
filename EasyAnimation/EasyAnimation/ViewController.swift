5787930648f6d387d3c0b595c162664933c82eaf
Khuong Pham <dkhuong291@gmail.com>
2016-03-28 09:58:35 +0700

    Create Project EasyAnimation

---
+++ EasyAnimation/EasyAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyAnimation
+//
+//  Created by Khuong Pham on 3/28/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
