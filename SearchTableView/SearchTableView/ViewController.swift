f17cef6cbb8e8acc09200f7c64cc14d8a247ae00
Khuong Pham <dkhuong291@gmail.com>
2016-03-30 14:30:48 +0700

    Finish Project SearchTableView

--- SearchTableView/SearchTableView/ViewController.swift
+++ SearchTableView/SearchTableView/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UISearchResultsUpdating {
+
+    @IBOutlet var tableView: UITableView!
+
+    let tableData = ["One", "Two", "Three", "Four", "Five", "Six"]
+    var filteredTableData = [String]()
+    var resultSearchController = UISearchController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        resultSearchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+
+            self.tableView.tableHeaderView = controller.searchBar
+
+            return controller
+        })()
+
+        self.tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultSearchController.active ? filteredTableData.count : tableData.count
     }
 
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        if resultSearchController.active {
+            cell.textLabel?.text = filteredTableData[indexPath.row]
+            return cell
+        } else {
+            cell.textLabel?.text = tableData[indexPath.row]
+            return cell
+        }
+    }
+
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        filteredTableData.removeAll(keepCapacity: false)
+
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (tableData as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        filteredTableData = array as! [String]
+
+        tableView.reloadData()
+    }
 }
 
