c7953673e169b67b97aec8164a18801edbc3c5ad
Khuong Pham <dkhuong291@gmail.com>
2016-03-31 19:14:41 +0700

    Create Project SwipeViewController

---
+++ SwipeViewController/SwipeViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeViewController
+//
+//  Created by Khuong Pham on 3/31/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
