2e4ca38bcdbb2c7f3d60cb4104a022bb1819688e
Khuong Pham <dkhuong291@gmail.com>
2016-04-01 14:00:01 +0700

    Finish Project CacheImageUsingNSUserDefault

--- CacheImageUsingNSUserDefault/CacheImageUsingNSUserDefault/ViewController.swift
+++ CacheImageUsingNSUserDefault/CacheImageUsingNSUserDefault/ViewController.swift
@@ -11,17 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
+    let userDefault = NSUserDefaults.standardUserDefaults()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let imageData = userDefault.objectForKey("khuong") as? NSData
+        if let imageData = imageData {
+            let image = UIImage(data: imageData)
+            imageView.image = image
+        }
     }
 
 
     @IBAction func downloadButtonTapped(sender: AnyObject) {
-        let url = NSURL(string: "https://avatars0.githubusercontent.com/u/15992374?v=3&s=200")
+        let url = NSURL(string: "https://avatars3.githubusercontent.com/u/11523438?v=3&s=460")
         let data = NSData(contentsOfURL: url!)
         let image = UIImage(data: data!)
         imageView.image = image
+        if let image = image {
+            let imageData = UIImagePNGRepresentation(image)
+            userDefault.setObject(imageData, forKey: "khuong")
+        }
     }
 }
 
