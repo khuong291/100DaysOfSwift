3caae978f838fcb7486e71f6893db5aac0817d6b
Khuong Pham <dkhuong291@gmail.com>
2016-04-01 12:09:46 +0700

    Finish Project UsingNSFileManager

--- UsingNSFileManager.playground/Contents.swift
+++ UsingNSFileManager.playground/Contents.swift
@@ -2,6 +2,17 @@
 
 import UIKit
 
+class BigEnderShield: NSObject, NSFileManagerDelegate {
+    func fileManager(fileManager: NSFileManager, shouldRemoveItemAtURL URL: NSURL) -> Bool {
+        var shouldDelete = true
+        let urlString = URL.absoluteString
+        if urlString.rangeOfString("bigender") != nil {
+            shouldDelete = false
+        }
+        return shouldDelete
+    }
+}
+
 let fileManager = NSFileManager.defaultManager()
 do {
     let document = try fileManager.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
@@ -15,6 +26,33 @@ do {
     for url in directoryListing {
         print(url)
     }
+
+    let bigEnderDirectory = document.URLByAppendingPathComponent("bigender")
+    let littleEnders = document.URLByAppendingPathComponent("littleender")
+
+    try? fileManager.createDirectoryAtURL(bigEnderDirectory, withIntermediateDirectories: false, attributes: nil)
+    try? fileManager.createDirectoryAtURL(littleEnders, withIntermediateDirectories: false, attributes: nil)
+
+    let bigEnderShield = BigEnderShield()
+    fileManager.delegate = bigEnderShield
+
+    for index in 0...6 {
+        var destinationURL: NSURL
+        let fileName = "string_\(index).txt"
+        let targetItem = document.URLByAppendingPathComponent(fileName)
+
+        if index % 2 == 0 {
+            destinationURL = bigEnderDirectory
+        } else {
+            destinationURL = littleEnders
+        }
+
+        destinationURL = destinationURL.URLByAppendingPathComponent(fileName)
+
+        try fileManager.removeItemAtURL(destinationURL)
+
+    }
+
 } catch {
     print(error)
 }
