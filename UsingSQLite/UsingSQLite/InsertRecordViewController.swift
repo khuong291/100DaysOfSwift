083a60b654f4a86f24abc825af5bb2742a6890a9
Khuong Pham <dkhuong291@gmail.com>
2016-04-02 19:15:08 +0700

    Create Layout

---
+++ UsingSQLite/UsingSQLite/InsertRecordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InsertRecordViewController.swift
+//  UsingSQLite
+//
+//  Created by Khuong Pham on 4/2/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class InsertRecordViewController: UIViewController {
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var marksTextField: UITextField!
+    @IBAction func saveButtonTapped(sender: AnyObject) {
+    }
+    @IBAction func backButtonTapped(sender: AnyObject) {
+    }
+}
---
+++ UsingSQLite/UsingSQLite/StudentCell.swift
@@ -0,0 +1,17 @@
+//
+//  StudentCell.swift
+//  UsingSQLite
+//
+//  Created by Khuong Pham on 4/2/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class StudentCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var markLabel: UILabel!
+    @IBOutlet var editButton: UIButton!
+    @IBOutlet var deleteButton: UIButton!
+}
