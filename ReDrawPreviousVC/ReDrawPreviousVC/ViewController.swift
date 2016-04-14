02b6cfbcfd9c668891dd01020946b511e5a2064d
Khuong Pham <dkhuong291@gmail.com>
2016-04-14 22:22:19 +0700

    Create Project ReDrawPreviousVC

---
+++ ReDrawPreviousVC/ReDrawPreviousVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReDrawPreviousVC
+//
+//  Created by Khuong Pham on 4/14/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
