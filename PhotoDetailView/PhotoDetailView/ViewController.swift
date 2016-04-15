7a76400d0ddc4c81af096b023423d6f660f74da4
Khuong Pham <dkhuong291@gmail.com>
2016-04-15 19:23:13 +0700

    Finish Project PhotoDetailView

--- PhotoDetailView/PhotoDetailView/ViewController.swift
+++ PhotoDetailView/PhotoDetailView/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    var barIsHidden = false
+    var navigationBarHeight: CGFloat = 0
+    var tabBarHeight: CGFloat = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.hideAndShowBar))
+        view.addGestureRecognizer(tapGesture)
+        navigationBarHeight = (self.navigationController?.navigationBar.frame.size.height)!
+        tabBarHeight = (self.tabBarController?.tabBar.frame.size.height)!
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func hideAndShowBar() {
+        print("tap!!")
+        if barIsHidden == false {
+            UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseOut, animations: {
+                // fade animation
+                self.navigationController?.navigationBar.alpha = 0.0
+                self.tabBarController?.tabBar.alpha = 0.0
+                // set height animation
+                self.navigationController?.navigationBar.frame.size.height = 0.0
+                self.tabBarController?.tabBar.frame.size.height = 0.0
+                }, completion: { (_) in
+                    self.barIsHidden = true
+            })
+        } else {
+            UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseOut, animations: {
+                // fade animation
+                self.navigationController?.navigationBar.alpha = 1.0
+                self.tabBarController?.tabBar.alpha = 1.0
+                // set height animation
+                self.navigationController?.navigationBar.frame.size.height = self.navigationBarHeight
+                self.tabBarController?.tabBar.frame.size.height = self.tabBarHeight
+                }, completion: { (_) in
+                    self.barIsHidden = false
+            })
+        }
+    }
 
 }
 
