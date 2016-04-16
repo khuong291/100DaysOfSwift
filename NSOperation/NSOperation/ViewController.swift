0a1449b04cdf79d2d8631e8bbda0904cc261d718
Khuong Pham <dkhuong291@gmail.com>
2016-04-16 22:11:47 +0700

    Finish Project NSOperation

--- NSOperation/NSOperation/ViewController.swift
+++ NSOperation/NSOperation/ViewController.swift
@@ -34,6 +34,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var sliderValueLabel: UILabel!
 
+    var queue = NSOperationQueue()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -46,36 +48,59 @@ class ViewController: UIViewController {
 
     @IBAction func didClickOnStart(sender: AnyObject) {
 
-        let concurrentQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-        let serialQueue = dispatch_queue_create("khuong", DISPATCH_QUEUE_SERIAL)
-
-        dispatch_async(serialQueue) {
+        queue = NSOperationQueue()
+        let operation1 = NSBlockOperation(block: {
             let img1 = Downloader.downloadImageWithURL(imageURLs[0])
-            dispatch_async(dispatch_get_main_queue()) {
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView1.image = img1
-            }
+            })
+        })
+
+        operation1.completionBlock = {
+            print("Operation 1 completed")
         }
 
-        dispatch_async(serialQueue) {
+        let operation2 = NSBlockOperation(block: {
             let img2 = Downloader.downloadImageWithURL(imageURLs[1])
-            dispatch_async(dispatch_get_main_queue()) {
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView2.image = img2
-            }
+            })
+        })
+
+        operation2.completionBlock = {
+            print("Operation 2 completed")
         }
 
-        dispatch_async(serialQueue) {
+        let operation3 = NSBlockOperation(block: {
             let img3 = Downloader.downloadImageWithURL(imageURLs[2])
-            dispatch_async(dispatch_get_main_queue()) {
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView3.image = img3
-            }
+            })
+        })
+
+        operation3.completionBlock = {
+            print("Operation 3 completed")
         }
 
-        dispatch_async(serialQueue) {
+        let operation4 = NSBlockOperation(block: {
             let img4 = Downloader.downloadImageWithURL(imageURLs[3])
-            dispatch_async(dispatch_get_main_queue()) {
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView4.image = img4
-            }
+            })
+        })
+        
+        operation4.completionBlock = {
+            print("Operation 4 completed")
         }
+
+        operation2.addDependency(operation3)
+        operation4.addDependency(operation2)
+        operation1.addDependency(operation4)
+
+        queue.addOperation(operation1)
+        queue.addOperation(operation2)
+        queue.addOperation(operation3)
+        queue.addOperation(operation4)
 
     }
     @IBAction func sliderValueChanged(sender: UISlider) {
