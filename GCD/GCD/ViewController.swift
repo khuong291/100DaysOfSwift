381904d2114d3d75df0187103e5149636fc41a6e
Khuong Pham <dkhuong291@gmail.com>
2016-04-16 11:24:42 +0700

    Create layout

--- GCD/GCD/ViewController.swift
+++ GCD/GCD/ViewController.swift
@@ -8,7 +8,31 @@
 
 import UIKit
 
+let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg",
+                 "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg",
+                 "http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg",
+                 "http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"]
+
+class Downloader {
+
+    class func downloadImageWithURL(url:String) -> UIImage! {
+
+        let data = NSData(contentsOfURL: NSURL(string: url)!)
+        return UIImage(data: data!)
+    }
+}
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView1: UIImageView!
+
+    @IBOutlet weak var imageView2: UIImageView!
+
+    @IBOutlet weak var imageView3: UIImageView!
+
+    @IBOutlet weak var imageView4: UIImageView!
+
+    @IBOutlet weak var sliderValueLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +44,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didClickOnStart(sender: AnyObject) {
 
+        let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+        self.imageView1.image = img1
+
+        let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+        self.imageView2.image = img2
+
+        let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+        self.imageView3.image = img3
+
+        let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+        self.imageView4.image = img4
+
+    }
+    @IBAction func sliderValueChanged(sender: UISlider) {
+
+        self.sliderValueLabel.text = "\(sender.value * 100.0)"
+    }
+    
 }
+
 
