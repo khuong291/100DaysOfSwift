af7af694ed6065255d48480534c3d2f0f7b3ef2d
Khuong Pham <dkhuong291@gmail.com>
2016-04-19 11:54:25 +0700

    Create Project DesignPatterns

---
+++ DesignPatterns/BlueLibrarySwift/Album.swift
@@ -0,0 +1,18 @@
+//
+//  Album.swift
+//  BlueLibrarySwift
+//
+//  Created by Khuong Pham on 4/19/16.
+//  Copyright © 2016 Raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class Album: NSObject {
+
+    var title: String!
+    var artist: String!
+    var genre: String!
+    var coverUrl: String!
+    var year: String!
+}
