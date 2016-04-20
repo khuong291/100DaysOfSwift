7ed2479f463b4960e047dae4321ff53ee836436b
Khuong Pham <dkhuong291@gmail.com>
2016-04-20 18:48:38 +0700

    Finish Project LimitCharacters

--- LimitCharacters/LimitCharacters/ViewController.swift
+++ LimitCharacters/LimitCharacters/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var textField2: UITextField!
+    @IBOutlet var textField1: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        textField1.delegate = self
+        textField2.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+
+        if textField == textField1 {
+            label1.text = "\(10 - textField1.text!.characters.count)"
+
+            if range.length > 10 {
+                return false
+            }
+
+            let newLength = textField1.text!.characters.count + range.length
+
+            return newLength < 10
+
+        } else if textField == textField2 {
+            label2.text = "\(8 - textField2.text!.characters.count)"
+
+            if range.length > 8 {
+                return false
+            }
+
+            let newLength = textField2.text!.characters.count + range.length
+            
+            return newLength < 8
+        }
+        return true
     }
-
-
+    
 }
 
