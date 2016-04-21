731f70d02af1198548929d50cdcac4317361c386
Khuong Pham <dkhuong291@gmail.com>
2016-04-21 23:10:09 +0700

    Create Project VisualFormatLanguage

---
+++ VisualFormatLanguage/VisualFormatLanguage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisualFormatLanguage
+//
+//  Created by Khuong Pham on 4/21/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
