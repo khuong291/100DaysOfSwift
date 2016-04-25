a7d2a1e73fc53e42b94a02ec0a2563d770629b94
Khuong Pham <dkhuong291@gmail.com>
2016-04-25 15:09:30 +0700

    Finish Project FunctionalProgramming

--- FunctionalProgramming.playground/Contents.swift
+++ FunctionalProgramming.playground/Contents.swift
@@ -2,8 +2,53 @@
 
 import UIKit
 
+var evens = [Int]()
+
 func isEven(number: Int) -> Bool {
     return number % 2 == 0
 }
-evens = Array(1...10).filter(isEven)
-println(evens)
+
+evens = Array(1...10).filter{ $0 % 2 == 0 }
+print(evens)
+
+//func reduce<U>(initial: U, combine: (U, T) -> U) -> U
+var evenSum = evens.reduce(0) { $0 + $1 }
+print(evenSum)
+
+let maxNumber = Array(1...10).reduce(0) { max($0, $1) }
+print(maxNumber)
+
+let numbers = Array(1...10).reduce("numbers: ") { $0 + "\($1)"}
+print(numbers)
+
+let digits = ["3", "1", "4", "1"]
+//let myDigit = Int(digits.reduce("") { $0 + $1 })
+let myDigit = digits.reduce(0) { 10 * $0 + Int($1, radix: 10)! }
+print(myDigit)
+
+let animals = ["Cat", "Chicken", "Fish", "Dog", "Mouse", "Pig", "Monkey"]
+typealias Entry = (Character, [String])
+
+func buildIndex(words: [String]) -> [Entry] {
+    var result = [Entry]()
+    var letters = [Character]()
+    for word in words {
+        let firstLetter = Character(word.substringToIndex(word.startIndex.advancedBy(1)).uppercaseString)
+        if !letters.contains(firstLetter) {
+            letters.append(firstLetter)
+        }
+    }
+    for letter in letters {
+        var wordsForLetter = [String]()
+        for word in words {
+            let firstLetter = Character(word.substringToIndex(word.startIndex.advancedBy(1)).uppercaseString)
+            if firstLetter == letter {
+                wordsForLetter.append(word)
+            }
+        }
+        result.append((letter, wordsForLetter))
+    }
+    return result
+}
+print(buildIndex(animals))
+
--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Here is a punch of example projects with Swift, I am going to build 100 example 
 
 [Youtube - Vea Software](https://www.youtube.com/user/veasoftware)
 
-Progress: 45/100
+Progress: 46/100
 
 ## Project Demos
 
@@ -207,6 +207,10 @@ Project 44 - [AddConstraintsProgrammatically](https://github.com/khuong291/Swift
 Project 45 - [AddConstraintsProgrammatically2](https://github.com/khuong291/Swift_Example_Series/tree/master/AddConstraintsProgrammatically2)
 
 ![Alt text](https://github.com/khuong291/Swift_Example_Series/blob/master/Demo%20Files/AddConstraintsProgrammatically2.png)
+
+Project 46 - [FunctionalProgramming](https://github.com/khuong291/Swift_Example_Series/tree/master/FunctionalProgramming)
+
+![Alt text](https://github.com/khuong291/Swift_Example_Series/blob/master/Demo%20Files/FunctionalProgramming.png)
 
 ## Reach me ##
 
