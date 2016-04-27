d5986e9e7f3aee6f5dd3e288467a2964e376beec
Khuong Pham <dkhuong291@gmail.com>
2016-04-27 17:49:04 +0700

    Finish Project

--- Generic.playground/Contents.swift
+++ Generic.playground/Contents.swift
@@ -2,14 +2,97 @@
 
 import UIKit
 
-func swapTwoInts(inout a: Int, inout _ b: Int) {
+func swapTwoValues<T>(inout a: T, inout _ b: T) {
     let temporaryA = a
     a = b
     b = temporaryA
 }
-
 var someInt = 3
 var anotherInt = 107
-swapTwoInts(&someInt, &anotherInt)
-print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
-// Prints "someInt is now 107, and anotherInt is now 3"
+swapTwoValues(&someInt, &anotherInt)
+
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("a")
+stackOfStrings.push("b")
+stackOfStrings.push("c")
+print(stackOfStrings)
+let fromTheTop = stackOfStrings.pop()
+
+extension Stack {
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+    mutating func append(item: Element) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Element {
+        return items[i]
+    }
+}
+
+if let topItem = stackOfStrings.topItem {
+    print("The top item on the stack is \(topItem).")
+}
+
+func findIndex<T: Equatable>(array: [T], _ valueToFind: T) -> Int? {
+    for (index, value) in array.enumerate() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+let doubleIndex = findIndex([3.14159, 0.1, 0.25], 9.3)
+let stringIndex = findIndex(["Mike", "Malcolm", "Andrea"], "Andrea")
+
+protocol Container {
+    associatedtype ItemType
+    mutating func append(item: ItemType)
+    var count: Int { get }
+    subscript(i: Int) -> ItemType { get }
+}
+
+extension Array: Container {}
+extension Stack: Container {}
+
+func allItemsMatch< C1: Container, C2: Container where C1.ItemType == C2.ItemType, C1.ItemType: Equatable> (someContainer: C1, _ anotherContainer: C2) -> Bool {
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
+    for i in 0..<someContainer.count {
+        if someContainer[i] != anotherContainer[i] {
+            return false
+        }
+    }
+    return true
+
+}
+
+var myStack = Stack<String>()
+myStack.push("a")
+myStack.push("b")
+myStack.push("c")
+
+var arrayOfStrings = ["a", "b", "c"]
+
+if allItemsMatch(myStack, arrayOfStrings) {
+    print("All items match.")
+} else {
+    print("Not all items match.")
+}
+
+
