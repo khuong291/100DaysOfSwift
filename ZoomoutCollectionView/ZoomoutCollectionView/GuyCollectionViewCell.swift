fc060e7bd9f590d158595c20d47a62566ce930cd
Khuong Pham <dkhuong291@gmail.com>
2016-04-30 22:25:23 +0700

    Create Project ZoomoutCollectionView

---
+++ ZoomoutCollectionView/ZoomoutCollectionView/GuyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GuyCollectionViewCell.swift
+//  ZoomoutCollectionView
+//
+//  Created by Khuong Pham on 4/30/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class GuyCollectionViewCell: UICollectionViewCell {
+    
+}
