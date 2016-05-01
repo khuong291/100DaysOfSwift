48fbf8f41c21e1d2fb70ddd3b3d2f60686241686
Khuong Pham <dkhuong291@gmail.com>
2016-05-01 13:45:05 +0700

    Add Images

--- FloatingButton/FloatingButton/ViewController.swift
+++ FloatingButton/FloatingButton/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var floatingButton: UIImageView!
+    var floatingButtonIsActive = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        addShadowToFloatingButton()
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(floatingButtonTapped(_:)))
+        floatingButton.addGestureRecognizer(gesture)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func addShadowToFloatingButton() {
+        floatingButton.layer.shadowColor = UIColor.grayColor().CGColor
+        floatingButton.layer.shadowOffset = CGSize(width: -2, height: 2)
+        floatingButton.layer.shadowOpacity = 1
+        floatingButton.layer.shadowRadius = 1
     }
 
+    func floatingButtonTapped(sender: UITapGestureRecognizer) {
 
+        if floatingButtonIsActive == false {
+
+            UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .CurveEaseInOut, animations: {
+                self.floatingButton.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_4))
+                }, completion: { _ in
+                    self.floatingButtonIsActive = true
+            })
+
+        } else {
+            UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .CurveEaseInOut, animations: {
+                self.floatingButton.transform = CGAffineTransformIdentity
+                }, completion: { _ in
+                    self.floatingButtonIsActive = false
+            })
+        }
+    }
 }
 
