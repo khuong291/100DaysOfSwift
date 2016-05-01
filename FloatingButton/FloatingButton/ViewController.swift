a3632f11f89d2ba6f9e60fc65c4e0f281f717c13
Khuong Pham <dkhuong291@gmail.com>
2016-05-01 14:02:55 +0700

    Animating

--- FloatingButton/FloatingButton/ViewController.swift
+++ FloatingButton/FloatingButton/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var floatingButtonTopToKhuongBottomConstraint: NSLayoutConstraint!
+    @IBOutlet var khuongTopToKhoaBottomConstraint: NSLayoutConstraint!
+
+    @IBOutlet var khoaImageView: UIImageView!
+    @IBOutlet var khuongImageView: UIImageView!
+    @IBOutlet var khuongView: UIView!
+    @IBOutlet var khoaView: UIView!
+
     @IBOutlet var floatingButton: UIImageView!
     var floatingButtonIsActive = false
 
@@ -19,6 +27,27 @@ class ViewController: UIViewController {
         addShadowToFloatingButton()
         let gesture = UITapGestureRecognizer(target: self, action: #selector(floatingButtonTapped(_:)))
         floatingButton.addGestureRecognizer(gesture)
+        makeCircleImages()
+        makeKhuongAndKhoaViewHidden()
+        floatingButtonTopToKhuongBottomConstraint.constant = 0
+        khuongTopToKhoaBottomConstraint.constant = 0
+    }
+
+    private func makeKhuongAndKhoaViewHidden() {
+        khoaView.hidden = true
+        khuongView.hidden = true
+    }
+
+    private func makeKhuongAndKhoaViewAppeared() {
+        khoaView.hidden = false
+        khuongView.hidden = false
+    }
+
+    private func makeCircleImages() {
+        khuongImageView.layer.cornerRadius = khuongImageView.frame.width / 2
+        khuongImageView.clipsToBounds = true
+        khoaImageView.layer.cornerRadius = khoaImageView.frame.width / 2
+        khoaImageView.clipsToBounds = true
     }
 
     private func addShadowToFloatingButton() {
@@ -31,16 +60,24 @@ class ViewController: UIViewController {
     func floatingButtonTapped(sender: UITapGestureRecognizer) {
 
         if floatingButtonIsActive == false {
+            makeKhuongAndKhoaViewAppeared()
 
             UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .CurveEaseInOut, animations: {
                 self.floatingButton.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_4))
+                self.floatingButtonTopToKhuongBottomConstraint.constant = 30
+                self.khuongTopToKhoaBottomConstraint.constant = 30
+                self.view.layoutIfNeeded()
                 }, completion: { _ in
                     self.floatingButtonIsActive = true
             })
 
         } else {
+            makeKhuongAndKhoaViewHidden()
             UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .CurveEaseInOut, animations: {
                 self.floatingButton.transform = CGAffineTransformIdentity
+                self.floatingButtonTopToKhuongBottomConstraint.constant = 0
+                self.khuongTopToKhoaBottomConstraint.constant = 0
+                self.view.layoutIfNeeded()
                 }, completion: { _ in
                     self.floatingButtonIsActive = false
             })
