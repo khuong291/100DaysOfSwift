4b07007d41f34a4b4a3acd1f0449abd260c44366
Khuong Pham <dkhuong291@gmail.com>
2016-05-01 12:39:08 +0700

    Create Project FloatingButton

---
+++ FloatingButton/FloatingButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FloatingButton
+//
+//  Created by Khuong Pham on 5/1/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
