9c6f1068cce5515bf7d5f027397541043f922551
Khuong Pham <dkhuong291@gmail.com>
2016-05-15 10:45:51 +0700

    Edit Code For Cell

--- Project54 - ExpandableTableView/ExpandableTableView/ExpandableTableView/MyTableViewCell.swift
+++ Project54 - ExpandableTableView/ExpandableTableView/ExpandableTableView/MyTableViewCell.swift
@@ -13,11 +13,36 @@ class MyTableViewCell: UITableViewCell {
     @IBOutlet var containerView: UIView!
     @IBOutlet var leftView: UIView!
 
+    class var expandedHeight: CGFloat { get { return 450 } }
+
+    class var defaultHeight: CGFloat { get { return 200 } }
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         containerView.layer.cornerRadius = 10
         containerView.clipsToBounds = true
+        leftView.layer.cornerRadius = 10
+        leftView.clipsToBounds = true
+    }
+
+    func checkHeight() {
+        
+    }
+
+    func watchFrameChange() {
+        addObserver(self, forKeyPath: "frame", options: .New, context: nil)
+        checkHeight()
+    }
+
+    func ignoreFrameChange() {
+        removeObserver(self, forKeyPath: "frame")
+    }
+
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if keyPath == "frame" {
+            checkHeight()
+        }
     }
 
 }
--- Project54 - ExpandableTableView/ExpandableTableView/ExpandableTableView/ViewController.swift
+++ Project54 - ExpandableTableView/ExpandableTableView/ExpandableTableView/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet var tableView: UITableView!
 
-    var isExpanded = false
+    var selectedIndexPath: NSIndexPath?
     let detailView = DetailView()
 
     override func viewDidLoad() {
@@ -30,7 +30,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 2
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -39,52 +39,67 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 200
+        return indexPath == selectedIndexPath ? MyTableViewCell.defaultHeight : MyTableViewCell.expandedHeight
     }
 
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if !isExpanded {
-            isExpanded = true
-
-            let cellContentView = tableView.cellForRowAtIndexPath(indexPath)?.contentView
-            let containerViewSize = cellContentView?.subviews.last?.bounds
-            detailView.frame.size = CGSize(width: (containerViewSize?.width)!, height: 0)
-
-            let rect = cellContentView!.convertRect(cellContentView!.frame, toView: self.view)
-            detailView.center = CGPointMake(rect.origin.x + rect.size.width / 2, rect.origin.y + rect.size.height / 2)
-            cellContentView!.addSubview(detailView)
-
-            UIView.transitionFromView(self.detailView, toView: self.detailView.contentView, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromTop, completion: nil)
-
-            UIView.animateWithDuration(0.5, delay: 0, options: [.CurveEaseInOut], animations: {
-                tableView.cellForRowAtIndexPath(indexPath)?.frame.size.height += 50
-                cellContentView?.frame.size.height += 20
-                self.detailView.center.y += 30
-                self.detailView.frame.size.height += (containerViewSize?.height)!
-                }, completion: nil)
-
+        let previousIndexPath = selectedIndexPath
+        if indexPath == selectedIndexPath {
+            selectedIndexPath = nil
         } else {
-            isExpanded = false
-
-            let cellContentView = tableView.cellForRowAtIndexPath(indexPath)?.contentView
-            let containerViewSize = cellContentView?.subviews.last?.bounds
-            detailView.frame.size = CGSize(width: (containerViewSize?.width)!, height: 0)
-
-            let rect = cellContentView!.convertRect(cellContentView!.frame, toView: self.view)
-            detailView.center = CGPointMake(rect.origin.x + rect.size.width / 2, rect.origin.y + rect.size.height / 2)
-
-            UIView.transitionFromView(self.detailView.contentView, toView: self.detailView, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromBottom, completion: nil)
-
-            UIView.animateWithDuration(0.5, delay: 0, options: [.CurveEaseInOut], animations: {
-                tableView.cellForRowAtIndexPath(indexPath)?.frame.size.height -= 50
-                cellContentView?.frame.size.height -= 20
-                self.detailView.center.y -= 30
-                self.detailView.frame.size.height -= (containerViewSize?.height)!
-                }, completion: { bool in
-                    self.detailView.removeFromSuperview()
-                    self.detailView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-            })
+            selectedIndexPath = indexPath
         }
+
+        var indexPaths: Array<NSIndexPath> = []
+        if let previous = previousIndexPath {
+            indexPaths += [previous]
+        }
+        if let current = selectedIndexPath {
+            indexPaths += [current]
+        }
+        if indexPaths.count > 0 {
+            tableView.reloadRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+        }
+//
+//            let cellContentView = tableView.cellForRowAtIndexPath(indexPath)?.contentView
+//            let containerViewSize = cellContentView?.subviews.last?.bounds
+//            detailView.frame.size = CGSize(width: (containerViewSize?.width)!, height: 0)
+//
+//            let rect = cellContentView!.convertRect(cellContentView!.frame, toView: self.view)
+//            detailView.center = CGPointMake(rect.origin.x + rect.size.width / 2, rect.origin.y + rect.size.height / 2)
+//            cellContentView!.addSubview(detailView)
+//
+//            UIView.transitionFromView(self.detailView, toView: self.detailView.contentView, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromTop, completion: nil)
+//
+//            UIView.animateWithDuration(0.5, delay: 0, options: [.CurveEaseInOut], animations: {
+//                tableView.cellForRowAtIndexPath(indexPath)?.frame.size.height += 50
+//                cellContentView?.frame.size.height += 20
+//                self.detailView.center.y += 30
+//                self.detailView.frame.size.height += (containerViewSize?.height)!
+//                }, completion: nil)
+//
+//        } else {
+//            isExpanded = false
+//
+//            let cellContentView = tableView.cellForRowAtIndexPath(indexPath)?.contentView
+//            let containerViewSize = cellContentView?.subviews.last?.bounds
+//            detailView.frame.size = CGSize(width: (containerViewSize?.width)!, height: 0)
+//
+//            let rect = cellContentView!.convertRect(cellContentView!.frame, toView: self.view)
+//            detailView.center = CGPointMake(rect.origin.x + rect.size.width / 2, rect.origin.y + rect.size.height / 2)
+//
+//            UIView.transitionFromView(self.detailView.contentView, toView: self.detailView, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromBottom, completion: nil)
+//
+//            UIView.animateWithDuration(0.5, delay: 0, options: [.CurveEaseInOut], animations: {
+//                tableView.cellForRowAtIndexPath(indexPath)?.frame.size.height -= 50
+//                cellContentView?.frame.size.height -= 20
+//                self.detailView.center.y -= 30
+//                self.detailView.frame.size.height -= (containerViewSize?.height)!
+//                }, completion: { bool in
+//                    self.detailView.removeFromSuperview()
+//                    self.detailView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+//            })
+//        }
     }
 
 }
