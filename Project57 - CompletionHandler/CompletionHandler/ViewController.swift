561064715b7c29dc3142dffb7c18696cb54731df
Khuong Pham <dkhuong291@gmail.com>
2016-05-22 19:18:11 +0700

    Finish Project CompletionHandler

--- Project57 - CompletionHandler/CompletionHandler/ViewController.swift
+++ Project57 - CompletionHandler/CompletionHandler/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var lineViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var lineView: UIView!
+    @IBOutlet var statusLabel: UILabel!
     @IBOutlet var downloadButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.lineViewHeightConstraint.constant = 0
+        statusLabel.hidden = true
 
         addBackgroundImage()
 
@@ -21,6 +28,11 @@ class ViewController: UIViewController {
         downloadButton.layer.borderColor = UIColor.whiteColor().CGColor
         downloadButton.layer.borderWidth = 1
         downloadButton.setTitle("Download\nImages", forState: .Normal)
+
+        statusLabel.layer.cornerRadius = 10
+        statusLabel.layer.borderColor = UIColor.whiteColor().CGColor
+        statusLabel.layer.borderWidth = 1
+
         downloadButton.titleLabel?.textAlignment = NSTextAlignment.Center
     }
 
@@ -35,8 +47,55 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor(patternImage: image)
     }
 
-    @IBAction func downloadButtonTapped(sender: AnyObject) {
+    func processDownloadImages(completion: () -> Void) {
+        var count = 0
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            if let url = NSURL(string: "https://github.com/khuong291/100DaysOfSwift/blob/master/cover_photo.png"), let _ = NSData(contentsOfURL: url) {
+                dispatch_async(dispatch_get_main_queue(), { 
+                    self.statusLabel.text = "Image1 is downloaded"
+                })
+                count += 1
+            }
+            if let url = NSURL(string: "http://sf.co.ua/13/07/wallpaper-2910355.jpg"), let _ = NSData(contentsOfURL: url) {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.statusLabel.text = "Image2 is downloaded"
+                })
+                count += 1
+            }
+            if let url = NSURL(string: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQv1W7ydicNeqtGQV_cBDwrjaPU1bpjhGL-muSjAryCvVme4k8V"), let _ = NSData(contentsOfURL: url) {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.statusLabel.text = "Image3 is downloaded"
+                })
+                count += 1
+                if count == 3 {
+                    completion()
+                }
+            }
+        }
+    }
 
+    @IBAction func downloadButtonTapped(sender: AnyObject) {
+        UIView.animateWithDuration(1.0, animations: {
+            self.lineViewHeightConstraint.constant = 100
+            self.view.layoutIfNeeded()
+            }, completion: { _ in
+                self.statusLabel.hidden = false
+        })
+        processDownloadImages {
+            dispatch_async(dispatch_get_main_queue(), { 
+                self.statusLabel.text = "All images are downloaded"
+                let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+                dispatch_after(delayTime, dispatch_get_main_queue()) {
+                    UIView.animateWithDuration(1.0, animations: { 
+                        self.lineViewHeightConstraint.constant = 0
+                        self.view.layoutIfNeeded()
+                        }, completion: { _ in
+                            self.statusLabel.hidden = true
+                    })
+                }
+
+            })
+        }
     }
 
 }
