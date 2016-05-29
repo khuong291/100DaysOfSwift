feab256224f5d22da730769fb26c9108203832d4
Khuong Pham <dkhuong291@gmail.com>
2016-05-29 12:35:37 +0700

    Create Project CallingAnimation

---
+++ Project59 - CallingAnimation/CallingAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CallingAnimation
+//
+//  Created by Khuong Pham on 5/29/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
