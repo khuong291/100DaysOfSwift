cf4ecf34a22a52eab3b5ec8f0ad55a15b2d34ab9
Khuong Pham <dkhuong291@gmail.com>
2016-05-29 21:57:32 +0700

    Create Project FishyLoadingView

---
+++ Project60 - FishyLoadingView/FishyLoadingView/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FishyLoadingView
+//
+//  Created by Khuong Pham on 5/29/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
