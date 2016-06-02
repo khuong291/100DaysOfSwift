a74322fa12d7c7e493b7bd342bbeb7b2b03d48a9
Khuong Pham <dkhuong291@gmail.com>
2016-06-02 12:56:50 +0700

    Create Project AttributedString

---
+++ Project61 - AttributedString/AttributedString/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AttributedString
+//
+//  Created by Khuong Pham on 6/2/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    static let NAME = "KHUONG"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
+
