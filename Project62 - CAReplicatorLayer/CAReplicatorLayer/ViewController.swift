365b6969b753c3f8aa33a7c6fbc18e1f23025670
Khuong Pham <dkhuong291@gmail.com>
2016-06-02 16:36:14 +0700

    Create Project CAReplicatorLayer

---
+++ Project62 - CAReplicatorLayer/CAReplicatorLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CAReplicatorLayer
+//
+//  Created by Khuong Pham on 6/2/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
