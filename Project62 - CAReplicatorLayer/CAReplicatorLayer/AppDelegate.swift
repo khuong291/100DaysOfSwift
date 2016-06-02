186b4d6f289d7867e7cb0045db50111ccf73252b
Khuong Pham <dkhuong291@gmail.com>
2016-06-02 20:00:13 +0700

    Finish Project CAReplicatorLayer

--- Project62 - CAReplicatorLayer/CAReplicatorLayer/AppDelegate.swift
+++ Project62 - CAReplicatorLayer/CAReplicatorLayer/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        application.statusBarHidden = true
         // Override point for customization after application launch.
         return true
     }
--- Project62 - CAReplicatorLayer/CAReplicatorLayer/ViewController.swift
+++ Project62 - CAReplicatorLayer/CAReplicatorLayer/ViewController.swift
@@ -15,28 +15,57 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // 1
+        createCircleLoadingView()
+        createThreeBarsLoadingView()
+    }
+
+    private func createThreeBarsLoadingView() {
+        let replicatorLayer = CAReplicatorLayer()
+        replicatorLayer.bounds = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+        replicatorLayer.position = view.center
+        replicatorLayer.backgroundColor = UIColor.clearColor().CGColor
+        view.layer.addSublayer(replicatorLayer)
+
+        let bar = CALayer()
+        bar.bounds = CGRect(x: 0.0, y: 0.0, width: 10.0, height: 50.0)
+        bar.position = CGPoint(x: 35.0, y: 75.0)
+        bar.cornerRadius = 2.0
+        bar.backgroundColor = UIColor.redColor().CGColor
+        
+        replicatorLayer.addSublayer(bar)
+
+        let move = CABasicAnimation(keyPath: "position.y")
+        move.toValue = bar.position.y - 35.0
+        move.duration = 0.5
+        move.autoreverses = true
+        move.repeatCount = Float.infinity
+
+        bar.addAnimation(move, forKey: nil)
+
+        replicatorLayer.instanceCount = 3
+        replicatorLayer.instanceTransform = CATransform3DMakeTranslation(20.0, 0.0, 0.0)
+        replicatorLayer.instanceDelay = 0.33
+        replicatorLayer.masksToBounds = true
+    }
+
+    private func createCircleLoadingView() {
         let replicatorLayer = CAReplicatorLayer()
         replicatorLayer.frame = loadingView.bounds
 
-        // 2
         replicatorLayer.instanceCount = 30
         replicatorLayer.instanceDelay = CFTimeInterval(1 / 50.0)
         replicatorLayer.preservesDepth = false
         replicatorLayer.instanceColor = UIColor.whiteColor().CGColor
 
-        // 3
         replicatorLayer.instanceRedOffset = -0.5
         replicatorLayer.instanceGreenOffset = 0.5
         replicatorLayer.instanceBlueOffset = -0.5
         replicatorLayer.instanceAlphaOffset = 1.0
 
-        // 4
         let angle = Float(M_PI * 2.0) / 30
         replicatorLayer.instanceTransform = CATransform3DMakeRotation(CGFloat(angle), 0.0, 0.0, 1.0)
         loadingView.layer.addSublayer(replicatorLayer)
 
-        // 5
         let instanceLayer = CALayer()
         let layerWidth: CGFloat = 10.0
         let midX = CGRectGetMidX(loadingView.bounds)
@@ -44,16 +73,16 @@ class ViewController: UIViewController {
         instanceLayer.backgroundColor = UIColor.whiteColor().CGColor
         replicatorLayer.addSublayer(instanceLayer)
 
-        // 6
         let fadeAnimation = CABasicAnimation(keyPath: "opacity")
         fadeAnimation.fromValue = 1.0
         fadeAnimation.toValue = 0.0
         fadeAnimation.duration = 1
         fadeAnimation.repeatCount = Float(Int.max)
-        
-        // 7
+
         instanceLayer.opacity = 0.0
         instanceLayer.addAnimation(fadeAnimation, forKey: "FadeAnimation")
+
     }
+
 }
 
