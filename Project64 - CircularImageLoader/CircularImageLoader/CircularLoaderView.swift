3f0141af983c86c5b5f4099b71d222fb56614296
Khuong Pham <dkhuong291@gmail.com>
2016-06-03 13:23:37 +0700

    Create Project CircularImageLoader

---
+++ Project64 - CircularImageLoader/CircularImageLoader/CircularLoaderView.swift
@@ -0,0 +1,13 @@
+//
+//  CircularLoaderView.swift
+//  CircularImageLoader
+//
+//  Created by Khuong Pham on 6/3/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class CircularLoaderView {
+    
+}
---
+++ Project64 - CircularImageLoader/CircularImageLoader/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CircularImageLoader
+//
+//  Created by Khuong Pham on 6/3/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
