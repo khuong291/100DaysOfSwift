cebebca08e66f5609fa734fda76332c99ab8c97c
Khuong Pham <dkhuong291@gmail.com>
2016-06-25 15:25:31 +0700

    Edit Project NSTextAttachmentCode

--- Project67 - NSTextAttachment/NSTextAttachment/ViewController.swift
+++ Project67 - NSTextAttachment/NSTextAttachment/ViewController.swift
@@ -17,14 +17,16 @@ class ViewController: UIViewController {
 
         let birdAttachment = NSTextAttachment()
         let birdImage = UIImage(named:"bird")
+        birdAttachment.bounds.size = CGSize(width: 20, height: 20)
         birdAttachment.image = birdImage
-        let birdString = NSAttributedString(string: "Bird")
+        let birdString = NSAttributedString(string: "Something really amazing happened in Downtown Spokane this week and I had to share the story with you. Some of you may know that my brother, Joel, is a loan officer at Sterling Bank. He works downtown in a second story office building, overlooking busy Riverside Avenue.")
         let stringWithBirdImage = NSAttributedString(attributedString: NSAttributedString(attachment: birdAttachment))
 
         let duckAttachment = NSTextAttachment()
         let duckImage = UIImage(named: "duck")
         duckAttachment.image = duckImage
-        let duckString = NSAttributedString(string: "Duck")
+        duckAttachment.bounds.size = CGSize(width: 20, height: 20)
+        let duckString = NSAttributedString(string: "Several weeks ago he watched a mother duck choose the cement awning outside his window as the uncanny place to build a nest above the sidewalk.The mallard laid ten eggs in a nest in the corner of the planter that is perched over 10 feet in the air. She dutifully kept the eggs warm for weeks and Monday afternoon all of her ten ducklings hatched.")
         let stringWithDuckImage = NSAttributedString(attributedString: NSAttributedString(attachment: duckAttachment))
 
         let finalAttributedString = NSMutableAttributedString(string: "")
