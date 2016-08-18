764d10b0c61d4622c4ac4f5c98b955de69743649
Khuong Pham <dkhuong291@gmail.com>
2016-08-18 15:27:33 +0700

    Create Project CustomRefreshControl

---
+++ Project70 - CustomRefreshControl/Project70 - CustomRefreshControl/TableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project70 - CustomRefreshControl
+//
+//  Created by Khuong Pham on 8/18/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
