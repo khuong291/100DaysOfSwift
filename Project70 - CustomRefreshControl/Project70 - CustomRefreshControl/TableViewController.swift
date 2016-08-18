fedb192bf64444af7a970bfcda4bb494678329d5
Khuong Pham <dkhuong291@gmail.com>
2016-08-18 15:55:13 +0700

    Finish Project CustomRefreshControl

--- Project70 - CustomRefreshControl/Project70 - CustomRefreshControl/TableViewController.swift
+++ Project70 - CustomRefreshControl/Project70 - CustomRefreshControl/TableViewController.swift
@@ -10,16 +10,58 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    var textArray = [String]()
+    var refresh = UIRefreshControl()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        textArray = ["You", "Are", "Awesome"]
+
+        refresh.tintColor = UIColor.clearColor()
+        refresh.backgroundColor = UIColor.clearColor()
+
+        loadRefreshControl()
+
+        self.tableView.addSubview(refresh)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func loadRefreshControl() {
+        let refreshContent = NSBundle.mainBundle().loadNibNamed("RefreshView", owner: self, options: nil)
 
+        let customView = refreshContent[0] as! UIView
+        customView.frame = refresh.bounds
+        customView.backgroundColor = UIColor.clearColor()
+
+        let customLabel = customView.viewWithTag(1) as! UILabel
+
+        customLabel.textColor = UIColor.whiteColor()
+        UIView.animateWithDuration(0.5, delay: 0, options: [.Autoreverse, .CurveEaseInOut, .Repeat], animations: {
+            customView.backgroundColor = UIColor.orangeColor()
+            customView.backgroundColor = UIColor.greenColor()
+            }, completion: nil)
+
+        self.refresh.addSubview(customView)
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textArray.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        cell.textLabel?.text = textArray[indexPath.row]
+
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        refresh.endRefreshing()
+    }
 }
 
