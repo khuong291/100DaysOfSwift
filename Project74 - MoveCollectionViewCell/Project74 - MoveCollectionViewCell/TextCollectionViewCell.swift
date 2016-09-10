e7ac1e5a86a769a34f586725b65d08076ac456fc
Khuong Pham <dkhuong291@gmail.com>
2016-09-10 23:11:28 +0700

    Finish Project MoveCollectionViewCell

---
+++ Project74 - MoveCollectionViewCell/Project74 - MoveCollectionViewCell/TextCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TextCollectionViewCell.swift
+//  Project74 - MoveCollectionViewCell
+//
+//  Created by Khuong Pham on 9/10/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class TextCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var textLabel: UILabel!
+}
