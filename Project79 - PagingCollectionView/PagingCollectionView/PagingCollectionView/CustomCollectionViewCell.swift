30a717cc0f675c4ffa834627c25363ed141c8951
Khuong Pham <dkhuong291@gmail.com>
2017-01-10 23:27:30 +0700

    Finish Project PagingCollectionView

---
+++ Project79 - PagingCollectionView/PagingCollectionView/PagingCollectionView/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  PagingCollectionView
+//
+//  Created by Khuong Pham on 1/10/17.
+//  Copyright © 2017 fantageek. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
